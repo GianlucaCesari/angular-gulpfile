439bb66302d05b1513a59a74e8ea7d8278caad3a
Gianluca Cesari <giangi.ces@gmail.com>
2016-10-17 08:57:40 +0200

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var ngmin = require('gulp-ngmin');
+var sass = require("gulp-sass");
+
+var paths = {
+    dev: {
+        sass: ['public/libs/css/*.scss'],
+        js: ['public/libs/js/main.js', 'public/libs/js/controllers/**.js']
+    },
+    dist: {
+        sass: {
+            name: 'base.css',
+            dest: 'public/dist/css'
+        },
+        js: {
+            name: 'app.js',
+            dest: 'public/dist/js'
+        }
+    }
+};
+
+gulp.task('js', function() {
+    return gulp.src(paths.dev.js)
+        .pipe(concat(paths.dist.js.name))
+        .pipe(ngmin())
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist.js.dest));
+});
+
+gulp.task('sass', function() {
+    return gulp.src(paths.dev.sass)
+        .pipe(concat(paths.dist.sass.name))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(paths.dist.sass.dest));
+});
+
+gulp.task('dist', function() {
+    // add your optimizations
+    console.log("Updated for distribution");
+});
+
+
+gulp.task('watch', function() {
+    console.log('js directory: ' + paths.dev.js.join(', '));
+    console.log('sass directory: ' + paths.dev.sass.join(', '));
+    gulp.watch(paths.dev.sass, ['sass']);
+    gulp.watch(paths.dev.js, ['js', 'dist']);
+});
